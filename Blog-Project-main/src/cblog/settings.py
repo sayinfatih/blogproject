a8fed19162af0c884d5c72003ecedcdd6f4e2c91
sayinfatih <129609044+sayinfatih@users.noreply.github.com>
2023-06-03 11:57:19 -0400

    Update settings.py

--- Blog-Project-main/src/cblog/settings.py
+++ Blog-Project-main/src/cblog/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'NAME': 'database1', # database name in RDS is written here
         'USER': 'admin', # database master username in RDS is written here
         'PASSWORD': config('PASSWORD'),
-        'HOST': 'database1.cewvxnnnahzx.us-east-1.rds.amazonaws.com',  # database endpoint is written here
+        'HOST': 'project-blog-db.csrmfmubd3nf.us-east-1.rds.amazonaws.com',  # database endpoint is written here
         'PORT': '3306' # database port is written here
     }
 }
@@ -144,7 +144,7 @@ LOGIN_REDIRECT_URL = "blog:list"
 LOGIN_URL = "login"
 
 
-AWS_STORAGE_BUCKET_NAME = 'project-bucket-techtorial' # please enter your s3 bucket name
+AWS_STORAGE_BUCKET_NAME = 'blog-s3-fatih' # please enter your s3 bucket name
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_REGION_NAME = "us-east-1" # please enter your s3 region 
 AWS_DEFAULT_ACL = 'public-read'
